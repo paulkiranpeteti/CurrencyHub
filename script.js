f91bc59c4323a09c6317414683be5a4eea4bab0c
petetipaulkiran <petetipaulkiran@gmail.com>
2025-06-26 11:12:49 +0530

    Figured out all the currencies and pushed into an array

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-const heading = document.querySelector("#heading");
-heading.textContent = "Currency Conversion Application";
+const apiKey = API.apiKey;
+const apiUrl = API.apiUrl;
+
+const symbols = [];
+const fetchSymbols = async () => {
+  const data = await fetch(apiUrl + "symbols" + "?access_key=" + apiKey);
+  const response = await data.json();
+  for(let key in response.symbols){
+    symbols.push(key);
+  }
+};
+
+fetchSymbols();
+console.log(symbols);
