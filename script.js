c213cece2d0ba9211d397ce807a36986f27fb338
petetipaulkiran <petetipaulkiran@gmail.com>
2025-06-27 15:19:40 +0530

    Included different types currencies into the option tag

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const apiKey = API.apiKey;
 const apiUrl = API.apiUrl;
 
-const symbols = [];
+const fromSelection = document.querySelector("#from");
+const toSelection = document.querySelector("#to");
+
 const fetchSymbols = async () => {
   const data = await fetch(apiUrl + "symbols" + "?access_key=" + apiKey);
   const response = await data.json();
-  for(let key in response.symbols){
-    symbols.push(key);
+  for (let key in response.symbols) {
+    const option1 = document.createElement("option");
+    option1.value = key;
+    option1.textContent = key;
+    fromSelection.appendChild(option1);
+
+    const option2 = document.createElement("option");
+    option2.value = key;
+    option2.textContent = key;
+    toSelection.appendChild(option2);
   }
 };
 
 fetchSymbols();
-console.log(symbols);
